ad15ca4b6a291673f9c4dd997c229d3680a44e44
George Yousif | Moddex <george.yousif@moddex.com>
2026-05-18 19:59:36 +1000

    chore(02-03): install @nestjs/schedule and wire ScheduleModule.forRoot() to AppModule
    - Added @nestjs/schedule@6.1.3 to api dependencies
    - Added ScheduleModule.forRoot() import to AppModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from './auth/auth.module';
 import { RtoModule } from './rto/rto.module';
 import { UsersModule } from './users/users.module';
+import { TgaModule } from './tga/tga.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
     AuthModule,
     RtoModule,
     UsersModule,
+    TgaModule,
   ],
 })
 export class AppModule {}
